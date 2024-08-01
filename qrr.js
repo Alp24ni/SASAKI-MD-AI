59fb7479610bdd02f2ad39fba48d651613e42bfa
𝕋𝕆𝔾𝔼 𝕀ℕ𝕌𝕄𝔸𝕂𝕀 <167923490+toge012345@users.noreply.github.com>
2024-08-01 11:31:28 +0100

    𝐒𝐀𝐒𝐀𝐊𝐈-𝐌𝐃

--- qrr.js
+++ qrr.js
@@ -17,14 +17,14 @@ const { Boom } = require("@hapi/boom");
 const PORT = process.env.PORT ||  5000
 const MESSAGE = process.env.MESSAGE ||  `
 ┌───⭓『
-❒ *WASI-MD*
+❒ *SASAKI-MD*
 ❒ _NOW DEPLOY IT_
 └────────────⭓
 ┌───⭓
 ❒  • Chat with owner •
-❒ *GitHub:* __https://github.com/WASI-MD_
-❒ *Author:* _wa.me/923192173398_
-❒ *YT:* _https://youtube.com/@wasitech10_
+❒ *GitHub:* _https://github.com/Alp24ni_
+❒ *Author:* _wa.me/24105114159_
+❒ *YT:* _https://youtube.com/@kenzo3146_
 └────────────⭓
 `
 
